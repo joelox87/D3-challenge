231f53a2196f9d2ecf7faacede052b561575a2de
joelox87 <j.aponte87@gmail.com>
2021-07-04 12:40:06 -0400

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
 // @TODO: YOUR CODE HERE!
+
+// Set up SVG area and chart margin dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 80
+};
